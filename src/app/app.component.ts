3c428c415a8cda574dd6bda40f8d26a332930c37
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2017-09-21 22:21:52 +0200

    added first programmatly animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {animate, AnimationBuilder, style} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private builder: AnimationBuilder) {
+
+  }
+
+  animate(theDiv:HTMLDivElement): void {
+    const animation = this.builder.build([
+      style({        backgroundColor:'red', width:'200px'}),
+      animate(300, style({width:'100%', backgroundColor:'red'})),
+      animate(300)
+    ]);
+    animation.create(theDiv).play();
+
+  }
 }
